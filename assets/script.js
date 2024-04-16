557f0ef51a6b14bd7cb26d78499717ecb7d45f3a
ahpoise <36297960+ahpoise@users.noreply.github.com>
2024-04-16 19:40:25 +0100

    update folder structure

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,5 @@ console.log("Ready to DOM! 🚀");
 
 const imgUrl = "https://source.unsplash.com/random/200x200?food&sig=";
 
+const body = document.querySelector("body");
 const container = document.querySelector("#posts-container");
